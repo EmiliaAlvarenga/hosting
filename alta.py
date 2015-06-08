61cee16cd09fb462b7a356500ab4eb668308de0e
root <root@hosting.example.com>
2015-06-08 20:03:38 +0200

    Correción de errores

--- alta.py
+++ alta.py
@@ -11,22 +11,21 @@ dominio=(sys.argv[2])
 import MySQLdb
 
 if os.path.isdir("/var/www/%s" % (usuario)) == False and os.path.isfile("/etc/apache2/sites-available/%s" % (dominio)) == False:
-	os.system("mkdir /var/www/%s" % (usuario))
+	os.system("mkdir /var/www/%s" % usuario)
+	os.system("echo Home Page %s is being Build  > /var/www/%s/index.html" %(usuario,usuario))
 else:
 	print "El usuario: %s o el dominio: %s ya existen, Elija otro usuario o nombre de dominio" % (usuario,dominio)
 	exit()
 
-
 #### Creacion del espacio del usuario en APACHE
-os.system("echo Home Page %s is being Build  > /var/www/%s/index.html" %(usuario,usuario))
 # abriremos el fichero en modo lectura para poder diseñar el nuevo virtualhost 
 virtualhost=open("plantillas/apache","r")
 lista=virtualhost.read()
 virtualhost.close()
 #usamos el metodo replace para poder remplazar al nuevo usuario en el modelo para el virtualhost 
-lista=lista.replace("@servername@","www.%s.com" % usuario)
-lista=lista.replace("@mail@","%s@gmail.com"% usuario)
-lista=lista.replace("@usuario@","%s"% usuario)
+lista=lista.replace("@servername@","%s.com" % usuario)
+lista=lista.replace("@mail@","%s@gmail.com" % usuario)
+lista=lista.replace("@usuario@","%s" % usuario)
 #abrimos el modo escritura y nos guarda el fichero del virtualhost creado para el usuario 
 virtualhost=open("/etc/apache2/sites-available/%s" % usuario,"w")
 virtualhost.write(lista)
@@ -46,7 +45,6 @@ phpmyadmin.close()
 os.system("a2ensite phpmyadmin%s >/dev/null" % usuario)
 # Reiniciamos apache
 os.system("service apache2 restart >/dev/null ")
-
 
 
 ### Configuración de DNS
@@ -77,41 +75,51 @@ os.system("service bind9 restart>/dev/null")
 #Generamos password para mysql
 def GenPasswd(n):
         return ''.join([choice(string.letters + string.digits) for i in range(n)])
+
+#Password para mysql
 passwdmysql = GenPasswd(8)
-#Generamos password para ftp
-def GenPasswd(n):
-        return ''.join([choice(string.letters + string.digits) for i in range(n)])
+#Password para ftp
 passwdftp = GenPasswd(8)
-conexionbd = MySQLdb.connect(host="localhost", user="root", passwd="root", db="ftpd")
+
+
+conexionbd = MySQLdb.connect(host="localhost", user="root", passwd="root")
 cursor = conexionbd.cursor()
 print "La contraseña de MySQL para el usuario %s es %s" % (usuario,passwdmysql)
+
 #creamos la base de datos para el usuario 
 bdusuario="create database %s" % (usuario)
 cursor.execute(bdusuario)
+
 #creamos el usuario y le damos todos los permisos sobre la base de datos que creamos  antes
 privilegessql= "grant all privileges on %s.* to"% (usuario)+ " %s@localhost"% (usuario)+ " identified by "+"'%s'" % (passwdmysql)
 cursor.execute(privilegessql)
+
 #guardamos los cambios
 dbreload = "FLUSH PRIVILEGES;"
 cursor.execute(dbreload)
+cursor.close()
+conexionbd.close()
+
+
 
 ### Configuración PROFTPD 
+conexionb= MySQLdb.connect(host="localhost",user="proftpd",passwd="proftpd",db="ftpd")
+cursor=conexionb.cursor()
 uid="select max(uid) from usuarios;"
 cursor.execute(uid)
 consultauid = cursor.fetchone()
 if consultauid[0] == None:
-	countuid=str(5001)
-        insertuser=" INSERT INTO usuarios VALUES ('"+usuario+"',"+"PASSWORD('"+passwdftp+"'),"+countuid+","+countuid+","+"'/var/www/"+usuario+"',"+"'/bin/false',"+"1,'"+dominio+ "');"
-	cursor.execute(insertuser)
-	conexionbd.commit()
-	os.system("chown -R %s:%s /var/www/%s" % (countuid,countuid,usuario))
+	countuid="2100"
 else:
-	countuid=consultauid[0]+1
-	countuidin=str(countuid)
-	insertuser=" INSERT INTO usuarios VALUES ('"+usuario+"',"+"PASSWORD('"+passwdftp+"'),"+countuidin+","+countuidin+","+"'/var/www/"+usuario+"',"+"'/bin/false',"+"1,'"+dominio+ "');"
-        cursor.execute(insertuser)
-	conexionbd.commit()
+	countuid=str(int(consultauid[0])+1)
+cursor.close()
+cursor = conexionb.cursor()
+insertuser="insert into usuarios values('"+usuario+"',password('"+passwdftp+"'),"+countuid+",2200,'/var/www/"+usuario+"','/bin/bash',1,'"+dominio+ "');"
+cursor.execute(insertuser)
+conexionb.commit()
+cursor.close()
 #Cambiamos el propietario de la carpeta creada en /var/www para cada usuario
-	os.system("chown -R %s:%s /var/www/%s" % (countuidin,countuidin,usuario))
-        print "La contraseña FTP para el usuario %s es %s" % (usuario,passwdftp)
+os.system('chown -R '+countuid+':www-data /var/www/'+usuario)
+os.system('chmod -R 770 /var/www/'+usuario)
+print "La contraseña FTP para el usuario %s es %s" % (usuario,passwdftp)
 print "El proceso se realizo satisfactoriamente"
